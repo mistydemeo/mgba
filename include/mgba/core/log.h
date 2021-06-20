dc57f47aef7cbf3c66716e7729fe38bd33786c7a
Misty De Meo <mistydemeo@gmail.com>
2021-06-19 19:01:40 -0700

    Import Medarot Classics Plus source

---
+++ include/mgba/core/log.h
@@ -0,0 +1,10 @@
+/* Copyright (c) 2013-2021 Jeffrey Pfau
+ *
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
+#pragma once
+#define mLOG(CATEGORY, LEVEL, ...)
+#define mLOG_DECLARE_CATEGORY(CATEGORY)
+#define mLOG_DEFINE_CATEGORY(CATEGORY, NAME, ID)
+#define mLog(CATEGORY, LEVEL, ...)
